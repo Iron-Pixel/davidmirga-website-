bea7ce1c1ca6be26aba3b4a7ca221afcdd91be80
David Mirga <69797891+Iron-Pixel@users.noreply.github.com>
2025-12-14 11:21:59 +0100

    SEO & Blog Update: Sitemap, Robots, Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind'; // Jetzt kennt er das!
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://davidmirga.com',
 
-  integrations: [
-    react(),
-    tailwind(), 
-  ],
+  integrations: [react(), tailwind(), sitemap()],
 });
